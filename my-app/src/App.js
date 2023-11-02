a1010663e4781997062c52f4f1d0575ac6dae358
kirtany <bhagwandinkirtanya@gmail.com>
2023-11-02 12:22:33 +0200

    edited html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Weather-Search-Engine</h1>
         <Search/>
       </header>
     </div>
